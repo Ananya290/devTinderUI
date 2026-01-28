99bee14f6a7e25d8b56e56f6ae65cdeb06d96c94
Ananya Shukla <ananyashukla.ss290@gmail.com>
2026-01-28 23:09:06 +0530

    Component and Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: 'home', loadComponent: () => import('../pages/home/home.component').then(m => m.HomeComponent) },
+    {path: 'auth', loadComponent: () => import('../pages/auth/auth.component').then(m => m.AuthComponent) },
+    {path: 'profile', loadComponent: () => import('../pages/profile/profile.component').then(m => m.ProfileComponent) },
+    {path: '**', loadComponent:()=> import('../pages/pagenotfound/pagenotfound.component').then(m => m.PagenotfoundComponent) }
+
+];
--- src/layout/header/header.component.ts
+++ src/layout/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatMenuModule, MatButtonModule , OverlayModule],
+  imports: [MatMenuModule, MatButtonModule, OverlayModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
